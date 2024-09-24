74771adee31d7f82d82c77e23a4e5faa347b949f
George Yegon <georgeyegon67@gmail.com>
2024-09-24 14:46:06 +0300

    Added Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42);
+  }
+  
+  
+  function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue);
+    return blocks * 264;
+  }
+  
+  
+  function distanceTravelledInFeet(start, destination) {
+    const distanceInBlocks = Math.abs(destination - start);
+    return distanceInBlocks * 264;
+  }
+  
+  
+  function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    
+    if (distanceInFeet <= 400) {
+      return 0; 
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      return (distanceInFeet - 400) * 0.02; 
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+      return 25; 
+    } else {
+      return 'cannot travel that far'; 
+    }
+  }
+console.log(distanceFromHqInBlocks(50)); 
+console.log(distanceFromHqInFeet(50)); 
+console.log(distanceTravelledInFeet(34, 38)); 
+console.log(calculatesFarePrice(34, 38));
+console.log(calculatesFarePrice(34, 42)); 
+console.log(calculatesFarePrice(34, 70)); 
